b9a530b1193c95d94c07756cbf5fff55805a52aa
Clyde Ancheta <clyde.a.p.ancheta@accenture.com>
2025-03-12 13:24:23 +0800

    +crappy gui mode

--- pptx-printer.py
+++ pptx-printer.py
@@ -5,6 +5,9 @@ from os import path, makedirs
 from pathlib import Path
 from textwrap import dedent
 from tqdm.auto import tqdm
+from tkinter import filedialog, Button, Tk, Label, StringVar
+from tkinter.messagebox import showinfo
+
 
 from win32com import client
 
@@ -17,13 +20,14 @@ def convert(input_folder, output_folder):
 
         for pptx_file in tqdm(files):
             pdf_file = Path(output_folder) / (str(pptx_file.stem) + ".pdf")
-            doc = powerpoint.Presentations.Open(str(pptx_file))
+            doc = powerpoint.Presentations.Open(str(pptx_file), WithWindow=False)
             try:
                 doc.SaveAs(str(pdf_file), FileFormat=ppSaveAsPDF)
             except:
                 raise
             finally:
                 doc.Close()
+
         powerpoint.Quit()
     except:
         raise
@@ -72,16 +76,57 @@ def commandline():
     convert(input_folder, output_folder)
 
 
+def gui():
+    root = Tk()
+    root.attributes("-topmost", True)
+    root.eval('tk::PlaceWindow . center')
+
+    input_folder = StringVar()
+    output_folder = StringVar()
+
+    input_label = Label(root,  textvariable=input_folder)
+    output_label = Label(root,  textvariable=output_folder)
+
+    def go_convert():
+        input = input_folder.get()
+        output = output_folder.get() or input
+
+        convert(input, output)
+        showinfo(message='Conversion complete.')
+
+    go = Button(root, text="Go", command=go_convert)
+
+    def select_output_folder():
+        folder = filedialog.askdirectory()
+        if folder:
+            output_folder.set(folder)
+            output_label.pack()
+
+    select_output = Button(root, text="Set", command=select_output_folder)
+
+    def select_input_folder():
+        folder = filedialog.askdirectory()
+        if folder:
+            input_folder.set(folder)
+            input_label.pack()
+            select_output.pack()
+            go.pack()
+
+    select_input = Button(root, text="Pick", command=select_input_folder)
+    select_input.pack()
+
+    root.mainloop()
+
+
 def run():
     if sys.platform != "win32":
         raise NotImplementedError("This program requires Microsoft Windows.")
 
     if len(sys.argv) == 1:
         # parser.print_help()
-        # gui mode
-        sys.exit(0)
-
-    commandline()
+        gui()
+    else:
+        commandline()
 
 
 run()
